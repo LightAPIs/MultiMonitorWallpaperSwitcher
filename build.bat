79a0cd0326e2499394a837793a755f3b1021c780
Light <47911535+LightAPIs@users.noreply.github.com>
2022-10-24 22:19:26 +0800

    chore(build): Display result file list

--- build.bat
+++ build.bat
@@ -7,12 +7,18 @@ set os=win
 
 echo ********** Build Release_x86 **********
 dotnet publish --configuration %configuration% --os %os% --arch x86 -p:PublishDir=..\publish\x86
+echo ********** Build Release_x86 Result **********
+tree /f .\publish\x86
 
 echo ********** Build Release_x64 **********
 dotnet publish --configuration %configuration% --os %os% --arch x64 -p:PublishDir=..\publish\x64
+echo ********** Build Release_x64 Result **********
+tree /f .\publish\x64
 
 echo ********** Build Release_arm64 **********
 dotnet publish --configuration %configuration% --os %os% --arch arm64 -p:PublishDir=..\publish\arm64
+echo ********** Build Release_arm64 Result **********
+tree /f .\publish\arm64
 
 echo ********** Build Done. **********
 REM pause>NUL
