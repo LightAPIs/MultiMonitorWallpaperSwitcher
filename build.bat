61aed2c366208b64fcdf2f51d5f7764b15e91d78
dragonish <no.web.developer@outlook.com>
2024-08-19 23:29:00 +0800

    build: rely on framework publish

--- build.bat
+++ build.bat
@@ -6,17 +6,17 @@ set configuration=Release
 set os=win
 
 echo ********** Build Release_x86 **********
-dotnet publish --configuration %configuration% --os %os% --arch x86 -p:PublishDir=..\publish\x86
+dotnet publish --self-contained false --configuration %configuration% --os %os% --arch x86 -p:PublishDir=..\publish\x86
 echo ********** Build Release_x86 Result **********
 tree /f .\publish\x86
 
 echo ********** Build Release_x64 **********
-dotnet publish --configuration %configuration% --os %os% --arch x64 -p:PublishDir=..\publish\x64
+dotnet publish --self-contained false --configuration %configuration% --os %os% --arch x64 -p:PublishDir=..\publish\x64
 echo ********** Build Release_x64 Result **********
 tree /f .\publish\x64
 
 echo ********** Build Release_arm64 **********
-dotnet publish --configuration %configuration% --os %os% --arch arm64 -p:PublishDir=..\publish\arm64
+dotnet publish --self-contained false --configuration %configuration% --os %os% --arch arm64 -p:PublishDir=..\publish\arm64
 echo ********** Build Release_arm64 Result **********
 tree /f .\publish\arm64
 
